b8b082e115a5710d912c6b3fd0015952c27b5c8d
Chance Smith <chancesmithb@gmail.com>
2018-08-24 14:03:48 -0500

    ⚡  IMPROVE: fix when data loads so dataset will fit in view

--- src/SHTimeline.js
+++ src/SHTimeline.js
@@ -3,57 +3,41 @@ import PropTypes from "prop-types";
 import Timeline from "react-visjs-timeline";
 
 class SHTimeline extends Component {
-  // http://visjs.org/docs/timeline/#Configuration_Options
-  state = {
-    query: "inet:dns:a | limit 100",
-    options: {
-      width: "100%",
-      height: "100vh",
-      stack: false,
-      showMajorLabels: true,
-      showCurrentTime: true,
-      // zoomMax: 315360000000000 / 10000,
-      zoomMin: 1000000,
-      type: "background",
-      format: {
-        minorLabels: {
-          minute: "h:mma",
-          hour: "ha"
-        }
-      }
-    },
-    items: [
-      {
-        start: new Date(2010, 7, 15),
-        end: new Date(2010, 8, 2), // end is optional
-        content: "Trajectory A"
-        // group: 1
+  constructor() {
+    super();
+    this.timelineWrapperRef = React.createRef();
+    this.state = {
+      loaded: false,
+      query: "inet:dns:a | limit 100",
+      options: {
+        // width: "100%",
+        height: "100vh"
+        // stack: true,
+        // showMajorLabels: true,
+        // showCurrentTime: true,
+        // // zoomMax: 315360000000000 / 10000,
+        // zoomMin: 1000000,
+        // type: "background",
+        // format: {
+        //   minorLabels: {
+        //     minute: "h:mma",
+        //     hour: "ha"
+        //   }
+        // },
+        // onUpdate: function(item, callback) {
+        //   console.log("update", item);
+        // }
       },
-      {
-        start: new Date(2010, 7, 20),
-        end: new Date(2010, 9, 13), // end is optional
-        content: "Trajectory B"
-        // group: 2
-      }
-    ],
-    groups: [
-      // {
-      //   id: 1,
-      //   content: "Group A"
-      // },
-      // {
-      //   id: 2,
-      //   content: "Group B"
-      // }
-    ]
-    // customTimes: {
-    //   one: new Date(),
-    //   two: "Tue May 10 2016 16:17:44 GMT+1000 (AEST)"
-    // }
-  };
+      items: []
+    };
+  }
+
+  // componentWillMount() {
+  // }
 
   componentDidMount() {
     this.loadData();
+    console.log(this.timelineWrapperRef.current);
   }
 
   params = obj => {
@@ -89,10 +73,13 @@ class SHTimeline extends Component {
             });
           }
         });
-        console.log(itemsArray);
         this.setState(previousState => ({
-          items: [...previousState.items, ...itemsArray]
+          items: itemsArray
         }));
+      })
+      .then(res => {
+        this.setState({ loaded: true });
+        // this.timelineWrapperRef.current.$el.fit();
       })
       .catch(err => console.error(this.state.query, err.toString()));
   }
@@ -100,12 +87,17 @@ class SHTimeline extends Component {
   render() {
     return (
       <div>
-        <Timeline
-          options={this.state.options}
-          items={this.state.items}
-          // groups={this.state.groups}
-          // customTimes={this.state.customTimes}
-        />
+        {this.state.loaded ? (
+          <Timeline
+            ref={this.timelineWrapperRef}
+            options={this.state.options}
+            items={this.state.items}
+            // groups={this.state.groups}
+            // customTimes={this.state.customTimes}
+          />
+        ) : (
+          <p>loading</p>
+        )}
       </div>
     );
   }
